11e3f0df0ef220bd48ba97123f91fba35da54d24
S&H-PC <alexaviergarcia89@gmail.com>
2024-04-18 03:51:26 -0500

    v1.1- Cleaned up some line comments.

--- cli.py
+++ cli.py
@@ -17,31 +17,31 @@ alarm_list = [item.strip() for item in alarm_list]  # Removes any whitespace fro
 # Defines the replacements in a dictionary.
 replacements = {'Site_Name': site, 'CP_Name': cp}
 
-# Processes each item in 'alarm_list'.
+# Processes each item in 'alarm_list' list.
 for alarm_name in alarm_list:
     # Reads the template XML file.
     with open('Alarm_TG_Template.xml', 'r') as xml_template_file:
         template_content = xml_template_file.read()
 
     # Replaces all occurrences of 'Site_Name' and 'CP_Name' from the text in the
-    # template file with the values from the 'replacements' dictionary.
+    # template file with the values from the 'replacements' dictionary. Also replaces
+    # all occurrences of 'Alarm_Name' from the text in the template file with the
+    # alarm names from the 'alarm_list' list.
     for old_text, new_text in replacements.items():
         template_content = template_content.replace(old_text, new_text)
 
-        # Replaces all occurrences of 'Alarm_Name' from the text in the
-        # Alarm_TG_Template.xml file with the alarm names from the 'alarm_list'.
         template_content = template_content.replace('Alarm_Name', alarm_name)
 
     # Writes the modified content to a new file.
     with open(f'{site}_{cp}_AlmTransGrp.xml', 'a') as xml_import_file:
-        xml_import_file.write(template_content)
+        xml_import_file.write(template_content + '\n')
 
 # Adds the <Project> & <Groups> tags to the beginning & end of the file.
 with open(f'{site}_{cp}_AlmTransGrp.xml', 'r') as xml_import_file:
     text = xml_import_file.read()
 
 with open(f'{site}_{cp}_AlmTransGrp.xml', 'w') as xml_import_file:
-    xml_import_file.write(f'<Project><Groups>{text}</Groups></Project>')
+    xml_import_file.write(f'<Project>\n<Groups>\n{text}</Groups>\n</Project>')
 
 # Closes the files.
 xml_template_file.close()
